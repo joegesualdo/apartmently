38682d2521894b5c5894316d68140093c33cd602
Joe Gesualdo <joegesualdo@gmail.com>
2013-10-27 09:27:16 -0400

    Get Address and Rental tests to pass

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131027130306) do
+ActiveRecord::Schema.define(version: 20131027131503) do
 
   create_table "addresses", force: true do |t|
     t.string   "line1"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20131027130306) do
     t.string   "zipcode"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "addressable_id"
+    t.string   "addressable_type"
   end
 
   create_table "rentals", force: true do |t|
