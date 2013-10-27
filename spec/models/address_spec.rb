103ff089da98f748972e074cfa9c3ffedc579aa6
Joe Gesualdo <joegesualdo@gmail.com>
2013-10-27 08:57:28 -0400

    Add shoulda specs for Address model

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Address do
-
+  it{should validate_presence_of(:line1)}
+  it{should validate_presence_of(:city)}
+  it{should validate_presence_of(:state)}
+  it{should validate_presence_of(:zipcode)}
 end
