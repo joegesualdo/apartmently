98d06c7d12ad200321d1d74de6610123219f4fe0
Joe Gesualdo <joegesualdo@gmail.com>
2013-10-27 12:00:05 -0400

    Add rental controller, view and routes; get all tests to pass

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Apartmently::Application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  resources :rentals
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
