70c2f96a173431323c4fefec14102dfb64dcd353
Joe Gesualdo <joegesualdo@gmail.com>
2013-10-27 09:52:43 -0400

    Add feature spec for homepage apartment viewing

--- spec/views/pages/home.html.erb_spec.rb
+++ spec/views/pages/home.html.erb_spec.rb
@@ -1,2 +1,9 @@
 require 'spec_helper'
 
+feature "Apartment viewing" do
+  scenario "User views apartments from homepage" do
+    visit root_path
+    click_link '2 Bedroom in Upper West Side'
+    expect(page).to have_text('2 Bedroom in Upper West Side')
+  end
+end
