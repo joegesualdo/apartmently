7a5b9c921c6ffeabd6a2300d372531e407c246ce
Joe Gesualdo <joegesualdo@gmail.com>
2013-10-27 09:43:27 -0400

    Add additional attributes to Rental model; make all tests pass

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,6 +2,10 @@ class Rental < ActiveRecord::Base
 
   has_one :address, as: :addressable
 
-  validates_presence_of :price
+  validates_presence_of :price,
+                        :cats,
+                        :dogs,
+                        :beds,
+                        :bathrooms
 
 end
