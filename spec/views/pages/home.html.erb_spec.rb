1bf83fa5b2fc74a1b9bcd83f4efbce783e6eb849
Joe Gesualdo <joegesualdo@gmail.com>
2013-10-28 09:41:46 -0400

    Try to get 'Apartment Viewing' feature to work.

--- spec/views/pages/home.html.erb_spec.rb
+++ spec/views/pages/home.html.erb_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 
 feature "Apartment viewing" do
   before(:each) do
-    FactoryGirl.build(:rental).save
+    rental = FactoryGirl.build(:rental).save
   end
   scenario "User views apartments from homepage" do
     visit root_path
-    click_link '100'
-    expect(page).to have_text('100')
+    click_link '2 bedrooms in Upper West Side'
+    expect(page).to have_text('2 bedrooms in Upper West Side')
   end
 end
