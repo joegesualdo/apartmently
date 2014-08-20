c3bc5c99a65283903022fe56448b50e7fea1645d
Joe Gesualdo <joegesualdo@gmail.com>
2014-08-20 19:35:17 -0400

    Update address.rb

---
+++ app/models/address.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id               :integer          not null, primary key
+#  line1            :string(255)
+#  line2            :string(255)
+#  city             :string(255)
+#  state            :string(255)
+#  zipcode          :string(255)
+#  created_at       :datetime
+#  updated_at       :datetime
+#  addressable_id   :integer
+#  addressable_type :string(255)
+
+
+class Address < ActiveRecord::Base
+
+  belongs_to :addressable, polymorphic: true
+
+  validates_presence_of :line1,
+                        :city,
+                        :state,
+                        :zipcode
+
+end
